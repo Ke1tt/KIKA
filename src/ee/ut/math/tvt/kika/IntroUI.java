48cbafbb397c667ded779ee249857620447af623
Kelian Kaio <kelian14@gmail.com>
2014-10-12 23:07:05 +0300

    Added Version Properties reading

--- src/ee/ut/math/tvt/kika/IntroUI.java
+++ src/ee/ut/math/tvt/kika/IntroUI.java
@@ -1,5 +1,11 @@
 package ee.ut.math.tvt.kika;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -8,7 +14,25 @@ import javafx.scene.layout.BorderPane;
 
 public class IntroUI{
 	
-		static Scene graafika(){
+		public String getVersionProps() throws IOException {
+			String result = "";
+			Properties prop = new Properties();
+			String propFileName= "version.properties";
+			
+			InputStream istream = new FileInputStream(propFileName);
+			prop.load(istream);
+		
+			String brn = prop.getProperty("build.revision.number");
+			String bmjn = prop.getProperty("build.minor.number");
+			String bman = prop.getProperty("build.major.number");
+			
+			result = brn + "." + bmjn + "." + bman;
+			
+			return result;
+		}
+		
+		static Scene graafika() throws IOException{
+
 		 BorderPane bp = new BorderPane();
 		 
 		 Label intro = new Label("Team name: KIKA" + "\n"
@@ -20,7 +44,8 @@ public class IntroUI{
 		 Label logo = new Label();
 		 logo.setGraphic(new ImageView(img));
 		 
-		 Label version = new Label("Software version number:");
+		 IntroUI verprops = new IntroUI();
+		 Label version = new Label("Software version number:" + verprops.getVersionProps());
 		 
 		 bp.setTop(intro);
 		 bp.setCenter(logo);
