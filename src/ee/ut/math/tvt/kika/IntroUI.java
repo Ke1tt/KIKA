1532bc420ba8143c669e08347c28ee0bca35f45f
Ingrid Kaasik <ingrid.kaasik@eesti.ee>
2014-10-12 22:05:05 +0300

    Version number almost done, only need to add version number to program

--- src/ee/ut/math/tvt/kika/IntroUI.java
+++ src/ee/ut/math/tvt/kika/IntroUI.java
@@ -5,7 +5,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
 
 public class IntroUI{
 	
