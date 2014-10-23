a3c051084cd53e7827d172295f925231d6a8d1a0
Ingrid Kaasik <ingrid.kaasik@eesti.ee>
2014-10-23 18:43:19 +0300

    Homework 4 things implemented to our code.

--- src/ee/ut/math/tvt/kika/Intro.java
+++ src/ee/ut/math/tvt/kika/Intro.java
@@ -1,25 +1,42 @@
 package ee.ut.math.tvt.kika;
 
-import javax.swing.SwingUtilities;
-
 import org.apache.log4j.Logger;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 
-public class Intro extends Application {
-	private static final Logger log = Logger.getLogger(Intro.class);
+public class Intro {
 
+private static final Logger log = Logger.getLogger(Intro.class);
+private static final String MODE = "console";
 
-	public static void main(String[] args) {
-		IntroUI intro = new IntroUI();
-		log.info("IntroUI created");
-		intro.setVisible(true);
+public static void main(String[] args) {
+
+	final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+	if (args.length == 1 && args[0].equals(MODE)) {
+		log.debug("Mode: " + MODE);
+
+		ConsoleUI cui = new ConsoleUI(domainController);
+		cui.run();
+	} else {
+
+		IntroUI introUI = new IntroUI();
+		introUI.setVisible(true);
+		introUI.setAlwaysOnTop(true);
+
+		final SalesSystemUI ui = new SalesSystemUI(domainController);
+		ui.setVisible(true);
+
+		introUI.setAlwaysOnTop(false);
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		introUI.setVisible(false);
 	}
-
-	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
+}
 };
