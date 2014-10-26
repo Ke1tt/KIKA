02da54e5814fbac92f0f5bcb17e0088e48143b49
Ingrid Kaasik <ingrid.kaasik@eesti.ee>
2014-10-26 19:46:35 +0200

    Needs a listener

--- src/ee/ut/math/tvt/salessystem/ui/model/ConfirmOrderModel.java
+++ src/ee/ut/math/tvt/salessystem/ui/model/ConfirmOrderModel.java
@@ -1,13 +1,15 @@
 package ee.ut.math.tvt.salessystem.ui.model;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import ee.ut.math.tvt.salessystem.ui.model.SalesSystemModel;
 
 public class ConfirmOrderModel extends JFrame {
 	
@@ -18,17 +20,23 @@ public class ConfirmOrderModel extends JFrame {
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(0,2));
 		
-		JLabel totalSum = new JLabel();
-		JLabel totalSumLabel = new JLabel("Total sum: " + total);
-		JTextField paidMoney = new JTextField();
+		JLabel totalSum = new JLabel(String.valueOf(total));
+		JLabel totalSumLabel = new JLabel("Total sum: ");
+		JFormattedTextField paidMoney = new JFormattedTextField();
 		JLabel paidMoneyLabel = new JLabel("Paid Money: ");
-		JLabel changeAmount = new JLabel();
+		//pmst sellega vist peab tegema
+		//paidMoney.addPropertyChangeListener());
+		double paidMoneyValue = Double.parseDouble(paidMoney.getText());
+		JLabel changeAmount = new JLabel(String.valueOf(total - paidMoneyValue));
 		JLabel changeAmountLabel = new JLabel("Change: ");
 		
-		panel.add(paidMoneyLabel);
-		panel.add(paidMoney);
+		
+		
+		
 		panel.add(totalSumLabel);
 		panel.add(totalSum);
+		panel.add(paidMoneyLabel);
+		panel.add(paidMoney);
 		panel.add(changeAmountLabel);
 		panel.add(changeAmount);
 		
