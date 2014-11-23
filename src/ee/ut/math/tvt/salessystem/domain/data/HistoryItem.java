9e716f1036a91bc6ecee9d287b26b8f1b79b5013
Kelian Kaio <kelian14@gmail.com>
2014-11-23 22:25:18 +0200

    One problem with historyItem solved

--- src/ee/ut/math/tvt/salessystem/domain/data/HistoryItem.java
+++ src/ee/ut/math/tvt/salessystem/domain/data/HistoryItem.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -16,6 +18,7 @@ import javax.persistence.Table;
 public class HistoryItem implements Cloneable, DisplayableItem {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "Id")
 	private int id;
 	@Column(name = "Price")
