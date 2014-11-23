dfb147bc651e00aff0ce99bac2dcae6af37eacfa
Kelian Kaio <kelian14@gmail.com>
2014-11-23 23:44:21 +0200

    Warehouse loads items from database - check

--- src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
+++ src/ee/ut/math/tvt/salessystem/domain/controller/impl/SalesDomainControllerImpl.java
@@ -37,17 +37,7 @@ public class SalesDomainControllerImpl implements SalesDomainController {
 
 	public List<StockItem> loadWarehouseState() {
 		// XXX mock implementation
-		List<StockItem> dataset = new ArrayList<StockItem>();
-
-		StockItem chips = new StockItem(1l, "Lays chips", "Potato chips", 11.0, 5);
-		StockItem chupaChups = new StockItem(2l, "Chupa-chups", "Sweets", 8.0, 8);
-	    StockItem frankfurters = new StockItem(3l, "Frankfurters", "Beer sauseges", 15.0, 12);
-	    StockItem beer = new StockItem(4l, "Free Beer", "Student's delight", 0.0, 100);
-
-		dataset.add(chips);
-		dataset.add(chupaChups);
-		dataset.add(frankfurters);
-		dataset.add(beer);
+		List<StockItem> dataset = session.createQuery("from StockItem").list();
 		
 		return dataset;
 	}
