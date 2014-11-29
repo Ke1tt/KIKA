9e837d35d57a0c6f3ff64a275a69a70922240451
Kelian Kaio <kelian14@gmail.com>
2014-11-29 15:28:34 +0200

    Created test classes

---
+++ task 1/test/ee/ut/math/tvt/SoldItemTest.java
@@ -0,0 +1,14 @@
+package ee.ut.math.tvt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SoldItemTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
