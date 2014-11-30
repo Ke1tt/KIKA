7257dfc5cfd69f96642c2933e78b515d4fca0e34
Kelian Kaio <kelian14@gmail.com>
2014-11-30 15:06:22 +0200

    Paar testi tehtud

--- task 1/test/ee/ut/math/tvt/PurchaseInfoTableModelTest.java
+++ task 1/test/ee/ut/math/tvt/PurchaseInfoTableModelTest.java
@@ -5,22 +5,57 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
 import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 import ee.ut.math.tvt.salessystem.ui.model.PurchaseInfoTableModel;
 
 public class PurchaseInfoTableModelTest {
 	
-	private PurchaseInfoTableModel model;
 	private StockItem item1;
+	private StockItem item2;
+	private SoldItem sold;
+	private SoldItem sold2;
 	
 	@Before
 	public void setUp() {
 		item1 = new StockItem((long) 1, "Porgand", "See on porgand", 2.30, 15);
+		item2 = new StockItem((long) 2, "Dipikaste", "See on porgandi jaoks", 3, 30);
+		sold = new SoldItem(item1, 2);
+		sold2 = new SoldItem(item2, 4);
 	}
 
 	@Test
-	public void test() {
-		assertFalse(model.checkQuantity(item1, 5));
+	public void testHasEnoughInStock() {
+		PurchaseInfoTableModel model = new PurchaseInfoTableModel();
+		assertTrue(model.checkQuantity(item1, 5));
+		assertFalse(model.checkQuantity(item1, 25));
+	}
+	
+	@Test
+	public void testAddSoldItem() {
+		PurchaseInfoTableModel model = new PurchaseInfoTableModel();
+		model.addItem(sold);
+		assertSame(model.getItemById(sold.getId()), sold);
+	}
+	
+	@Test
+	public void testGetSumWithNoItems() {
+		PurchaseInfoTableModel model = new PurchaseInfoTableModel();
+		assertEquals(model.totalSum(), 0, 0.001);
+	}
+	
+	@Test
+	public void testGetSumWithOneItem(){
+		PurchaseInfoTableModel model = new PurchaseInfoTableModel();
+		model.addItem(sold);
+		assertEquals(model.totalSum(), 4.60, 0.001);
 	}
 
+	@Test
+	public void testGetSumWithMultipleItems(){
+		PurchaseInfoTableModel model = new PurchaseInfoTableModel();
+		model.addItem(sold);
+		model.addItem(sold2);
+		assertEquals(model.totalSum(), 16.6, 0.001);
+	}
 }
