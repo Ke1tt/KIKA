6391f04eda9065d81702183b13a2c28c7b205cb5
Kelian Kaio <kelian14@gmail.com>
2014-11-30 13:14:44 +0200

    JUnit stuff.

---
+++ task 1/test/ee/ut/math/tvt/PurchaseInfoTableModelTest.java
@@ -0,0 +1,14 @@
+package ee.ut.math.tvt;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PurchaseInfoTableModelTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- task 1/test/ee/ut/math/tvt/SoldItemTest.java
+++ task 1/test/ee/ut/math/tvt/SoldItemTest.java
@@ -4,8 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class SoldItemTest {
+import ee.ut.math.tvt.salessystem.domain.data.SoldItem;
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
+public class SoldItemTest {
+	
+	@Before
+	public void setUp() {
+		stock1 = new StockItem(1, "Porgand", "See on porgand", 2.30, 15);
+		item1 = new SoldItem(stock1, 3);
+	}
 	@Test
 	public void test() {
 		fail("Not yet implemented");
