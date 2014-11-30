d73157b390f958176d999493d51be328dc40f362
Kelian Kaio <kelian14@gmail.com>
2014-11-30 14:02:29 +0200

    StockItem Basic Tests done

--- task 1/test/ee/ut/math/tvt/StockItemTest.java
+++ task 1/test/ee/ut/math/tvt/StockItemTest.java
@@ -2,13 +2,28 @@ package ee.ut.math.tvt;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
+
 public class StockItemTest {
+	
+	private StockItem item1;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() {
+		item1 = new StockItem((long) 1, "Porgand", "See on porgand", 2.30, 15);
 	}
-
+	
+	@Test
+	public void testClone() {
+		assertNotSame(item1.clone(),item1);
+	}
+	
+	@Test
+	public void testGetColumn(){
+		assertEquals("Porgand",item1.getColumn(1));
+	}
+	
 }
